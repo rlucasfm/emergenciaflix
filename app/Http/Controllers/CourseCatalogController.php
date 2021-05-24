84e01d5a09774d7e48b49cb96a192758a7565d47
rlucasfm <richardlucasfm@gmail.com>
2021-05-24 10:22:28 -0300

    First production Commit

--- app/Http/Controllers/CourseCatalogController.php
+++ app/Http/Controllers/CourseCatalogController.php
@@ -14,9 +14,10 @@ class CourseCatalogController extends Controller
         $data = [];
 
         $user_id = Auth::id();
-        $user_courses = UserCourse::where('user_id', $user_id)->get();
+        // $user_courses = UserCourse::where('user_id', $user_id)->get();
         $courses_aval = Course::join('user_course', 'courses.id', '=', 'user_course.course_id')
         ->select('courses.id', 'courses.name', 'courses.description', 'courses.link', 'courses.img', 'courses.sell_link', 'user_course.complete_lessons', 'user_course.completed')
+        ->where('user_course.user_id', '=', $user_id)
         ->get()
         ->toArray();
 
